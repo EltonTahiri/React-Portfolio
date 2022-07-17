1246d07a7ccea365904d2f0f23efad588449b3c0
EltonTahiri <eltontahiri4@gmail.com>
2022-07-17 16:41:16 +0200

    Added the skills compartment

--- src/components/skills/Skills.jsx
+++ src/components/skills/Skills.jsx
@@ -1,10 +1,71 @@
-import React from 'react'
-import './skills.css'
+import React from "react";
+import "./skills.css";
+import { HiBadgeCheck } from "react-icons/hi";
 
 const Skills = () => {
   return (
-    <section id='skills'>Skills</section>
-  )
-}
+    <section id="skills">
+      <h5>Coding Skills I Have</h5>
+      <h2>My Experience</h2>
 
-export default Skills
+      <div className="container experience__container">
+        <div className="experience__frontend">
+          <h3>Frontend Development</h3>
+          <div className="experience__content">
+            <article className="experience__details">
+              <HiBadgeCheck />
+              <h4>HTML</h4>
+              <small className="text-light">Experienced</small>
+            </article>
+
+            <article className="experience__details">
+              <HiBadgeCheck />
+              <h4>CSS</h4>
+              <small className="text-light">Experienced</small>
+            </article>
+
+            <article className="experience__details">
+              <HiBadgeCheck />
+              <h4>JavaScript</h4>
+              <small className="text-light">Being Developed</small>
+            </article>
+
+            <article className="experience__details">
+              <HiBadgeCheck />
+              <h4>React</h4>
+              <small className="text-light">Being Developed</small>
+            </article>
+          </div>
+        </div>
+
+ {/* end of frontend*/}
+
+        <div className="experience__backend">
+          <h3>Backend Development</h3>
+          <div className="experience__content">
+            <article className="experience__details">
+              <HiBadgeCheck />
+              <h4>Java</h4>
+              <small className="text-light">Basic</small>
+            </article>
+
+            <article className="experience__details">
+              <HiBadgeCheck />
+              <h4>.NET</h4>
+              <small className="text-light">Basic</small>
+            </article>
+
+            <article className="experience__details">
+              <HiBadgeCheck />
+              <h4>MSSQL</h4>
+              <small className="text-light">Basic</small>
+            </article>
+
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Skills;
