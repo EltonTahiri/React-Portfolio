ac6bdb6dc683b51034ebaa1c7432b022845c1107
EltonTahiri <eltontahiri4@gmail.com>
2022-07-17 18:15:15 +0200

    Portfolio Compartment

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,9 +1,71 @@
 import React from 'react'
 import './portfolio.css'
+import IMG1 from '../../assets/alarmclock.PNG'
+import IMG2 from '../../assets/counter.PNG'
+import IMG3 from '../../assets/JsCalc.PNG'
+import IMG4 from '../../assets/Quiz.PNG'
+import IMG5 from '../../assets/Quote.PNG'
+import IMG6 from '../../assets/Rent.PNG'
 
 const Portfolio = () => {
   return (
-    <section id='portfolio'>Portfolio</section>
+    <section id='portfolio'>
+      <h5>My Recent Works</h5>
+      <h2>Portfolio</h2>
+
+
+      <div className="container portfolio__container">
+        <article className='portfolio__item'>
+          <div className="portfolio__item-image">
+            <img src={IMG1} alt="Project 1"/ >
+          </div>
+          <h3>This is a portfolio item title</h3>
+          <a href="https://github.com" className='btn' target='_blank'>Github</a>
+        </article>
+
+        <article className='portfolio__item'>
+          <div className="portfolio__item-image">
+            <img src={IMG2} alt="Project 1"/ >
+          </div>
+          <h3>This is a portfolio item title</h3>
+          <a href="https://github.com" className='btn' target='_blank'>Github</a>
+        </article>
+
+        <article className='portfolio__item'>
+          <div className="portfolio__item-image">
+            <img src={IMG3} alt="Project 1"/ >
+          </div>
+          <h3>This is a portfolio item title</h3>
+          <a href="https://github.com" className='btn' target='_blank'>Github</a>
+        </article>
+
+        <article className='portfolio__item'>
+          <div className="portfolio__item-image">
+            <img src={IMG4} alt="Project 1"/ >
+          </div>
+          <h3>This is a portfolio item title</h3>
+          <a href="https://github.com" className='btn' target='_blank'>Github</a>
+        </article>
+
+        <article className='portfolio__item'>
+          <div className="portfolio__item-image">
+            <img src={IMG5} alt="Project 1"/ >
+          </div>
+          <h3>This is a portfolio item title</h3>
+          <a href="https://github.com" className='btn' target='_blank'>Github</a>
+        </article>
+
+        <article className='portfolio__item'>
+          <div className="portfolio__item-image">
+            <img src={IMG6} alt="Project 1"/ >
+          </div>
+          <h3>This is a portfolio item title</h3>
+          <a href="https://github.com" className='btn' target='_blank'>Github</a>
+        </article>
+
+
+      </div>
+    </section>
   )
 }
 
--- src/components/skills/Skills.jsx
+++ src/components/skills/Skills.jsx
@@ -13,68 +13,67 @@ const Skills = () => {
           <h3>Frontend Development</h3>
           <div className="experience__content">
             <article className="experience__details">
-              <HiBadgeCheck className='experience__details-icon' />
+              <HiBadgeCheck className="experience__details-icon" />
               <div>
-              <h4>HTML</h4>
-              <small className="text-light">Experienced</small>
+                <h4>HTML</h4>
+                <small className="text-light">Experienced</small>
               </div>
             </article>
 
             <article className="experience__details">
-              <HiBadgeCheck className='experience__details-icon' />
+              <HiBadgeCheck className="experience__details-icon" />
               <div>
-              <h4>CSS</h4>
-              <small className="text-light">Experienced</small>
+                <h4>CSS</h4>
+                <small className="text-light">Experienced</small>
               </div>
             </article>
 
             <article className="experience__details">
-              <HiBadgeCheck className='experience__details-icon' />
+              <HiBadgeCheck className="experience__details-icon" />
               <div>
-              <h4>JavaScript</h4>
-              <small className="text-light">Basic - Being Developed</small>
+                <h4>JavaScript</h4>
+                <small className="text-light">Basic - Being Developed</small>
               </div>
             </article>
 
             <article className="experience__details">
-              <HiBadgeCheck className='experience__details-icon' />
+              <HiBadgeCheck className="experience__details-icon" />
               <div>
-              <h4>React</h4>
-              <small className="text-light">Basic - Being Developed</small>
+                <h4>React</h4>
+                <small className="text-light">Basic - Being Developed</small>
               </div>
             </article>
           </div>
         </div>
 
- {/* end of frontend*/}
+        {/* end of frontend*/}
 
         <div className="experience__backend">
           <h3>Backend Development</h3>
           <div className="experience__content">
             <article className="experience__details">
-              <HiBadgeCheck className='experience__details-icon' />
+              <HiBadgeCheck className="experience__details-icon" />
               <div>
-              <h4>Java</h4>
-              <small className="text-light">Basic</small>
+                <h4>Java</h4>
+                <small className="text-light">Basic</small>
               </div>
             </article>
 
             <article className="experience__details">
-              <HiBadgeCheck className='experience__details-icon' />
+              <HiBadgeCheck className="experience__details-icon" />
               <div>
-              <h4>.NET</h4>
-              <small className="text-light">Basic</small>
+                <h4>.NET</h4>
+                <small className="text-light">Basic</small>
               </div>
             </article>
 
             <article className="experience__details">
-              <HiBadgeCheck className='experience__details-icon'  />
+              <HiBadgeCheck className="experience__details-icon" />
               <div>
-              <h4>MSSQL</h4>
-              <small className="text-light">Basic</small>
+                <h4>MSSQL</h4>
+                <small className="text-light">Basic</small>
               </div>
             </article>
-
           </div>
         </div>
       </div>
